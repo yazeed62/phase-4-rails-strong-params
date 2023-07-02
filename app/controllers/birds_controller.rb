179ec4a987e17d6f25962df7494db639d47562c1
Yazeed <dee3ayahmed25@gmail.com>
2023-07-02 22:53:41 +0300

    completed assingment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,5 @@
 class BirdsController < ApplicationController
+   wrap_parameters format: []
 
   # GET /birds
   def index
@@ -8,8 +9,14 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
   # GET /birds/:id
